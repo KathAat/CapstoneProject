2195b57949e0e281de14e7d09c3d0d4d76611ecd
Logan-Gunnin <lgunnin2@students.kennesaw.edu>
2023-10-05 15:05:30 -0400

    Added Get and Tested
    Added a get for the API then just displayed the data as is to confirm that it worked properly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+// Get JSON file, import from angular//
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import {OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Audit-Trail-Viewer';
-}
+
+  export class AppComponent implements OnInit {
+    jsonData: any;
+    title = 'Audit-Trail-Viewer';
+    constructor(private http: HttpClient) {}
+  
+    ngOnInit(): void {
+      this.http.get<any>('https://run.mocky.io/v3/0d105fe2-0e98-47a6-af35-161b5972035f').subscribe(data => {
+        this.jsonData = data;
+      });
+    }  
+  }
